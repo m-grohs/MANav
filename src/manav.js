021368a408e2cf66bd5fcfab1bbfaad36cb7f7e0
mgrohs <marc.grohs@gmail.com>
2023-08-31 13:20:06 +0200

    added base changes

--- src/manav.js
+++ src/manav.js
@@ -1,10 +1,17 @@
 'use strict';
 
-const extensionActive = false;
+const extensionIsActive = {
+	status: false
+};
 
 /**
  * React to Messages from service worker/background.js
  */
 browser.runtime.onMessage.addListener((msg) => {
 	console.log(msg);
+	extensionIsActive.status = msg;
 });
+
+if (extensionIsActive.status === true) {
+	console.log('Extensions is Active');
+}
